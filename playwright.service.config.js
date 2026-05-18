80521d680ff859ac531313f870ae0e2860380b2b
Prathisha-K <167665735+Prathisha-K@users.noreply.github.com>
2026-05-19 01:25:44 +0530

    Enable GitHub OIDC in Playwright config

--- playwright.service.config.js
+++ playwright.service.config.js
@@ -10,6 +10,7 @@ export default defineConfig(
     exposeNetwork: '<loopback>',
     connectTimeout: 3 * 60 * 1000, // 3 minutes
     os: ServiceOS.LINUX,
+    useGitHubOIDC: tru
     //credential: new DefaultAzureCredential(),
   }),
   {
